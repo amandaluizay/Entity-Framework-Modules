0b40327edd1d329743d8c8d0bf0b069bfda912ca
amanda <luiza.amandasilvah@gmail.com>
2022-08-17 15:49:55 -0300

    modulo1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Blog.Data;
+using Blog.Models;
+
+namespace Blog {
+    class program{
+        static void Main(string[] args){
+            using(var context= new BlogDataContext()){
+                
+                //create
+                /* var tag = new Tag {Name="ASP.NET", Slug="aspnet"};
+                context.Tags.Add(tag);
+                context.SaveChanges(); */
+
+               // Delete
+                /* var tag = context.Tags.FirstOrDefault(x=> x.Id == 1);
+                context.Remove(tag);
+                context.SaveChanges(); */
+
+                var tags = context.Tags.ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+            }
+
+        }
+    }
+}
